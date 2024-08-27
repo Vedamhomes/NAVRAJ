e7c6a7b7633761ba00fa94d2db0a576d79a5edd1
Saurabhv1584 <saurabhv1584@gmail.com>
2024-08-28 01:42:48 +0530

    changes

--- src/coreStrength.js
+++ src/coreStrength.js
@@ -20,9 +20,7 @@ const { setIsInputModalOpen } = props;
           Don't Miss Out. Enquire Today and Schedule Your Site Visit.
         </div>
 
-        <div className="info_box_number">
-          <div onClick={() => setIsInputModalOpen(true)} className="info_box_number_text">Call 9871534959</div>
-        </div>
+          <button onClick={() => setIsInputModalOpen(true)} className="info_box_number info_box_number_text">Call 9871534959</button>
       </div>
     </div>
   );
