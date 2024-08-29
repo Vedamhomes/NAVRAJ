fff14043e70d8f94e0849f7d48b909b7e3bf47a7
Saurabhv1584 <saurabhv1584@gmail.com>
2024-08-30 01:40:12 +0530

    legacy page

--- src/Legacy/index.js
+++ src/Legacy/index.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import style from './style.css'
-import Navbar from '../navbar'
-import Footer from '../footer'
-import UserInputModal from '../UserInputModal'
+import React, { useState } from "react";
+import style from "./style.css";
+import Navbar from "../navbar";
+import Footer from "../footer";
+import UserInputModal from "../UserInputModal";
 
 const Legacy = () => {
   const [isInputModalOpen, setIsInputModalOpen] = useState(false);
@@ -23,6 +23,49 @@ const Legacy = () => {
         />
       </div>
 
+      <div className="legacy_container">
+        <section className="info_container">
+          <div className="info_container_left">
+            <h1 className="info_container_left_heading">
+              It’s all about well-thought spaces in life.
+            </h1>
+            <img
+              className="leadership_info_image"
+              fetchpriority="high"
+              decoding="async"
+              src="https://navrajindia.com/wp-content/uploads/2024/05/Raj-1-758x564.jpg"
+              alt=""
+            />
+          </div>
+          <div className="info_container_right">
+            <div className="info_sub_heading_content">
+              <span className="info_sub_heading_content_one">
+                We at Navraj Infratech understand the importance of
+                well-thought-of space in life and have been providing our
+                customers with quality living spaces that are designed, putting
+                intense thought on the utility for the customers by making their
+                everyday life easy and delightful, in the cosmopolitan city of
+                the National Capital Region, Gurugram.
+              </span>
+              <span className="info_sub_heading_content_one">
+                We consider the entire length and breadth of the city as a
+                canvas which can be painted with many ideas when it comes to
+                creating bigger residential and commercial spaces at competitive
+                costs.
+              </span>
+              <span className="info_sub_heading_content_two">
+                With robust experience in land-aggregation and construction of
+                judiciously-designed residential projects in the secondary
+                market of Gurugram, we have now stepped into the primary market
+                of Gurugram with 2 life-based premium residential projects to
+                begin with. Our approach is based on the belief that our
+                customers deserve bigger ideas and bigger spaces that they can
+                call home.
+              </span>
+            </div>
+          </div>
+        </section>
+      </div>
       <Footer />
 
       {isInputModalOpen && (
@@ -32,7 +75,7 @@ const Legacy = () => {
         />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Legacy
+export default Legacy;
