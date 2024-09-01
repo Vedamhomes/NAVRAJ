9cd884dec7ee5e6ad2d8eeeacdc32873765e9620
Saurabhv1584 <saurabhv1584@gmail.com>
2024-09-01 16:46:02 +0530

    changes for project page image

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -20,12 +20,9 @@ const Projects = () => {
           className="main_image"
           fetchpriority="high"
           decoding="async"
-          src="//v3.navrajindia.com/wp-content/uploads/2024/05/banner_main_1.jpg"
+          src="navraj_project_page.png"
           alt=""
-          data-lazyload="//v3.navrajindia.com/wp-content/uploads/2024/05/banner_main_1.jpg"
-          data-no-retina=""
-          data-src-rs-ref="//v3.navrajindia.com/wp-content/uploads/2024/05/banner_main_1.jpg"
-        />
+       />
       </div>
 
       <div className='project_info_box'>
