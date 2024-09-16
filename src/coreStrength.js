9248240088c541d911a2e1c7d96cec5ae7f46c1e
Vedamhomes <info@vedamhomes.com>
2024-09-16 16:24:47 +0530

    Update coreStrength.js

--- src/coreStrength.js
+++ src/coreStrength.js
@@ -39,13 +39,12 @@ const location = useLocation();
         <div className='map_section2'>
             <div className='map_section2_cont'>
                 <h3> Navraj Head Office</h3>
-                <p class='desc'>614-616, 6th Floor, Tower 4,
-                    </br>DLF Corporate Greens, Sector-74A,
-                    </br>Gurugram (HR) - 122004
-                    </br><b>Phone: 0124-5180996<br></b>
-                    <b>Sales:</b> sales@navrajindia.com
-                    </br>
-                    <b>Support:</b> customercare@navrajindia.com</p>
+                 <p class='desc'>614-616, 6th Floor, Tower 4,
+                    <br>DLF Corporate Greens, Sector-74A,
+                    <br>Gurugram (HR) - 122004
+                    <br><b>Phone: 0124-5180996</b>
+                    <br><b>Sales:</b> sales@navrajindia.com
+                    <br><b>Support:</b> customercare@navrajindia.com</p>
             </div>
             <div className='map_section2_cont'>
                 <h3> Navraj - Faridabad Office</h3>
