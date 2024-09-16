9c8a0c384705fa4fcc6e1590c96b955707a302db
Vedamhomes <info@vedamhomes.com>
2024-09-16 12:59:39 +0530

    Update coreStrength.js

--- src/coreStrength.js
+++ src/coreStrength.js
@@ -13,17 +13,17 @@ const location = useLocation();
   return (
     <div className="core_strength_container">
       <div className='image_container'>
-        <img className='main_image_3' fetchpriority="high" decoding="async" src="natural_light_room_img.jpg" alt="" data-no-retina=""/>
+        <img className='main_image_3' fetchpriority="high" decoding="async" src="natural_light_room_img.jpg" alt="Natural Light Room" data-no-retina=""/>
         <div onClick={() => setIsInputModalOpen(true)} className='main_image_4_box'>
           <button className='main_image_4'>Click to know structural online</button>
           {/* <img className='main_image_4' fetchpriority="high" decoding="async" src="button2.png" alt="" data-no-retina=""/> */}
         </div>
       </div>
       <div className='image_container'>
-        <img className='main_image_2' fetchpriority="high" decoding="async" src="MarketingDocket_page-0004.jpg" alt="" data-no-retina=""/>
+        <img className='main_image_2' fetchpriority="high" decoding="async" src="MarketingDocket_page-0004.jpg" alt="Marketing Docket" data-no-retina=""/>
       </div>
       <div className='image_container'>
-        <img className='main_image' fetchpriority="high" decoding="async" src="MarketingDocket_page-0005.jpg" alt="" data-no-retina=""/>
+        <img className='main_image' fetchpriority="high" decoding="async" src="MarketingDocket_page-0005.jpg" alt="The Pinnacle of Opulence" data-no-retina=""/>
       </div>
       {location.pathname === '/projects' && (<div className='image_container'>
         <img className='main_image_2 main_image_3' fetchpriority="high" decoding="async" src="our_core_strength01.png" alt="" data-no-retina=""/>
