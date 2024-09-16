3f1381bbdefb792cc77fffc1d6196d25bb096182
Vedamhomes <info@vedamhomes.com>
2024-09-16 16:40:43 +0530

    Update coreStrength.js

--- src/coreStrength.js
+++ src/coreStrength.js
@@ -30,9 +30,9 @@ const location = useLocation();
       </div>)}
 	      {/*custom code for map*/}
 	<div className='image_container'>
-    <div className='main_map_sect first_section'>
+    <div className='main_map_sect'>
         <div className='map_section1'>
-            <iframe width="600" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27674.261708734954!2d76.97294566934833!3d28.40546213372811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3d44da25ec83%3A0x1e488481e254e73a!2sTower%204%2C%20DLF%20CORPORATE%20GREENS%2C%20Sector%2074A%2C%20Gurugram%2C%20Haryana%20122018!5e0!3m2!1sen!2sin!4v1726479336782!5m2!1sen!2sin"
+            <iframe width="800" height="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27674.261708734954!2d76.97294566934833!3d28.40546213372811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3d44da25ec83%3A0x1e488481e254e73a!2sTower%204%2C%20DLF%20CORPORATE%20GREENS%2C%20Sector%2074A%2C%20Gurugram%2C%20Haryana%20122018!5e0!3m2!1sen!2sin!4v1726479336782!5m2!1sen!2sin"
             title="Navraj Infratech" frameBorder="0">
             </iframe>
         </div>
