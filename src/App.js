2b4dd0503098c519c85b77d49045d898a2e5fae2
Vedamhomes <info@vedamhomes.com>
2024-09-17 11:41:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Home from "./Home";
 import Projects from './Projects';
 import Legacy from './Legacy';
 import Leadership from './Leadership.js';
-import ImageGallery from '.ImageGallery';
 
 function App() {
   const [isInputModalOpen, setIsInputModalOpen] = useState(false);
