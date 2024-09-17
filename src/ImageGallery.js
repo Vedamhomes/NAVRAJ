50807f8412192bd427699672abaf25ae32bc357c
Vedamhomes <info@vedamhomes.com>
2024-09-17 14:19:28 +0530

    Update ImageGallery.js

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -8,6 +8,8 @@ const images = [
   { src: 'antalyas_gallery_003.jpg', alt: 'Image 1' },
   { src: 'antalyas_gallery_004.jpg', alt: 'Image 2' },
   { src: 'antalyas_gallery_005.jpg', alt: 'Image 3' },
+   { src: 'antalyas_gallery_006.jpg', alt: 'Image 3' },
+   { src: 'antalyas_gallery_007.jpg', alt: 'Image 3' },
   // Add more images as needed
 ];
 
@@ -16,8 +18,8 @@ const ImageGallery = () => {
     <div className="gallery">
       {images.map((image, index) => (
         <div key={index} className="gallery-item">
-           <a href={require(`./${image.src}`)} data-lightbox="gallery" data-title={image.title}>
-            <img src={require(`./${image.src}`)} alt={image.alt} />
+           <a href={image.src} data-lightbox="gallery" data-title={image.title}>
+            <img src={image.src} alt={image.alt} />
           </a>
         </div>
       ))}
