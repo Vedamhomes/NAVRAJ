2c7787d8c7d4b7ecbbcb58569fc6ee1d2a4a4c5b
Vedamhomes <info@vedamhomes.com>
2024-09-17 12:11:13 +0530

    Update coreStrength.js

--- src/coreStrength.js
+++ src/coreStrength.js
@@ -41,9 +41,15 @@ const location = useLocation();
           </div>
 	</div>
 
-		<a href="https://api.whatsapp.com/send?phone=92668 11009" className="whatsapp-float"> whatapp</a>
 	 <div id="mainGallery"></div>
-      
+       <a
+      href="https://api.whatsapp.com/send?phone=9266811009&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202."
+      className="whatsapp-float"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i className="fa fa-whatsapp my-float"></i>
+    </a>
     
 	 { /* end custom code for map */}
 		    
