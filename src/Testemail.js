a50a96da86aa164fc9ddc36b2810192c6d3f58ab
Vedamhomes <info@vedamhomes.com>
2024-09-23 15:38:00 +0530

    Update Testemail.js

--- src/Testemail.js
+++ src/Testemail.js
@@ -29,15 +29,15 @@ const Testemail = () => {
   return (
    <form ref={form} onSubmit={sendEmail}>
       <label>First Name</label>
-      <input type="text" name="first_name" />
+      <input type="text" name="first_name"  required/>
     <label>Last Name</label>
-      <input type="text" name="last_name" />
+      <input type="text" name="last_name"  required/>
       <label>Email</label>
       <input type="email" name="email_address" required/>
     <label>Phone number</label>
-      <input type="number" name="phone_number" />
+      <input type="number" name="phone_number"  required/>
       <label>Property location</label>
-      <input  type="text" name="property_location" />
+      <input  type="text" name="property_location" required/>
       <input type="submit" value="Send" />
     </form>
   );
