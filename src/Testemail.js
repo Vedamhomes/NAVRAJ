25757f9c20a10e6bd01f098e032c1f479f44743b
Vedamhomes <info@vedamhomes.com>
2024-09-23 14:19:05 +0530

    Update Testemail.js

--- src/Testemail.js
+++ src/Testemail.js
@@ -13,7 +13,7 @@ export const ContactUs = () => {
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          console.log('SUCCESS!..');
         },
         (error) => {
           console.log('FAILED...', error.text);
